9a96bc8f2caaac3cfd413a7082528f56626d0da3
alikeli <eli-com2@yandex.ru>
2023-03-14 10:12:02 +0100

    done

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,22 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    protected String color;
+    protected int numberOfPaws;
+    protected boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+
+            return "This animal is mostly " + color + ". It has " + numberOfPaws + (numberOfPaws > 1 ? " paws" : " paw") + " and " + (hasFur ? "a" : "no") + " fur.";
+
+    }
+
+//   Add a methods getDescription(), witch would use class fields and return a string with such pattern
+//   "This animal is mostly (color). It has (numberOfPaws) paws and ('a'/'no' -> depends on value of hasFur) fur."
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,21 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal{
+//    private String color;
+//    private int numberOfPaws;
+//    private boolean hasFur;
+
+
+    public Bird() {
+        super("blue",
+                2 ,
+               false);
+    }
+
+    @Override
+    public String getDescription() {
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + (numberOfPaws > 1 ? " paws" : " paw") + " and " + (hasFur ? "a" : "no") + " fur. Moreover, it has 2 wings and can fly.";
+    }
+
 
 }
